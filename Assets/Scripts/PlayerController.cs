21662f4b8c3d67461ed2652b08286f036236bf46
torpedo87 <bestofhand@naver.com>
2018-08-19 00:43:55 +0900

    move and shoot

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed = 50.0f;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //xyz position
+        Vector3 pos = transform.position;
+        pos.x += moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+        pos.z += moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
+
+        //새 위치 적용
+        transform.position = pos;
+	}
+
+}
